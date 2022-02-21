d9c8c0487c42807d062d3dc3bed06ba14bc09769
DGUHJH <ghkdwogud852@gmail.com>
2022-02-21 21:20:58 +0900

    Fix : CommonImage 컴포넌트 height 설정 오류 수정

--- src/components/Image/Common/styled.ts
+++ src/components/Image/Common/styled.ts
@@ -3,7 +3,7 @@ import { ImageType } from 'types/common';
 
 export const Root = styled.img<ImageType>`
   width: ${(props) => props.width};
-  height: ${(props) => props.width ?? props.height};
+  height: ${(props) => props.height ?? 'auto'};
   margin-top: ${(props) => props.marginTop ?? 0}px;
   margin-bottom: ${(props) => props.marginBottom ?? 0}px;
   margin-left: ${(props) => props.marginLeft ?? 0}px;
