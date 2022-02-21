7c7d597086e6992f27d3ae6cb45a8bb557da9b28
DGUHJH <ghkdwogud852@gmail.com>
2022-02-21 20:19:14 +0900

    Feat : common types 추가

---
+++ src/types/common.ts
@@ -0,0 +1,47 @@
+export type FontType = {
+  fontFamily:
+    | 'NotoSansKR-Black'
+    | 'NotoSansKR-Bold'
+    | 'NotoSansKR-Medium'
+    | 'NotoSansKR-Regular'
+    | 'NotoSansKR-Thin'
+    | 'NanumSquareLight'
+    | 'NanumSquare'
+    | 'NanumSquareBold'
+    | 'NanumSquareExtraBold'
+    | 'NanumSquareAcb'
+    | 'NanumSquareAceb'
+    | 'NanumSquareAcl'
+    | 'NanumSquareAcr';
+  fontSize: number;
+  fontWeight?: number;
+  color?: string;
+  lineHeight?: string;
+  marginTop?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
+  onClick?: () => void;
+};
+
+export type ImageType = {
+  src: string;
+  width: number;
+  height?: number;
+  borderRadius?: number;
+  marginTop?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
+  onClick?: () => void;
+};
+
+export type DividerType = {
+  width: number;
+  height?: number;
+  background: string;
+  marginTop?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
+};
