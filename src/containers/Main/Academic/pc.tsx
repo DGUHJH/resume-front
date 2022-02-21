82af7fcdcf244df7bac8927c75b0022bbdde73e9
DGUHJH <ghkdwogud852@gmail.com>
2022-02-21 23:17:25 +0900

    Feat : MainAcademicContainer 컴포넌트 Data 추가

--- src/containers/Main/Academic/pc.tsx
+++ src/containers/Main/Academic/pc.tsx
@@ -37,7 +37,8 @@ const MainAcademicContainer = () => {
           lineHeight="180%"
           marginLeft={10}
         >
-          전반적인 computer science에 관한 내용에 대하여 배우고 있습니다.
+          전반적인 computer science에 관한 내용에 대하여 배우고 있습니다. 학점은
+          4.45로 1년 넘게 학과 1등을 지키고 있습니다.
         </CommonTypography>
       </Styled.AcademicContainer>
     </Styled.Root>
