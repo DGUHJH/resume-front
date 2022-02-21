239ac78a412d2a6fc3ca73e32052e9c8f3a5099b
DGUHJH <ghkdwogud852@gmail.com>
2022-02-21 22:00:06 +0900

    Feat : CommonDot 컴포넌트 margin 활성화

--- src/components/Dot/Common/styled.ts
+++ src/components/Dot/Common/styled.ts
@@ -6,4 +6,8 @@ export const Root = styled.div<DotType>`
   height: ${(props) => props.height ?? props.width}px;
   background: ${(props) => props.background};
   border-radius: ${(props) => props.width}px;
+  margin-top: ${(props) => props.marginTop ?? 0}px;
+  margin-bottom: ${(props) => props.marginBottom ?? 0}px;
+  margin-left: ${(props) => props.marginLeft ?? 0}px;
+  margin-right: ${(props) => props.marginRight ?? 0}px;
 `;
