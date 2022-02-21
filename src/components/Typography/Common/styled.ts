0b1ce57745696da06dff9e0e9c3b39c2d1741a72
DGUHJH <ghkdwogud852@gmail.com>
2022-02-21 22:00:35 +0900

    Style : CommonTypography 컴포넌트 children center 정렬

--- src/components/Typography/Common/styled.ts
+++ src/components/Typography/Common/styled.ts
@@ -9,6 +9,8 @@ export const Root = styled(Typography)<FontType>`
     font-weight: ${(props) => props.fontWeight ?? 'inherit'};
     color: ${(props) => props.color ?? '#000'};
     line-height: ${(props) => props.lineHeight ?? '120%'};
+    display: flex;
+    align-items: center;
     margin-top: ${(props) => props.marginTop ?? 0}px;
     margin-bottom: ${(props) => props.marginBottom ?? 0}px;
     margin-left: ${(props) => props.marginLeft ?? 0}px;
