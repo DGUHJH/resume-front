97af1cc5550410384f565e3bef78b6a058a868b3
DGUHJH <ghkdwogud852@gmail.com>
2022-02-21 23:19:31 +0900

    Feat : 모바일 예외 처리

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -1,10 +1,14 @@
 import Main from 'pages/pc';
+import { isMobile } from 'react-device-detect';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Main />} />
-    </Routes>
-  </BrowserRouter>
-);
+const App: React.FC = () =>
+  !isMobile ? (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  ) : (
+    <div>모바일은 지원하지 않습니다.</div>
+  );
 export default App;
